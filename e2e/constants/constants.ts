e9d8f601a76783c7f7c0654485ef3f81804c13ac
duyhungtnn <duyhung71089@gmail.com>
2025-09-25 17:11:28 +0700

    test: init e2e test

---
+++ e2e/constants/constants.ts
@@ -0,0 +1,15 @@
+export const HOST = process.env.HOST!;
+export const TOKEN = process.env.TOKEN!;
+export const SERVER_ROLE_TOKEN = process.env.SERVER_ROLE_TOKEN!;
+export const FEATURE_TAG = 'nodejs'
+export const TARGETED_USER_ID = 'bucketeer-nodejs-server-user-id-1'
+export const TARGETED_SEGMENT_USER_ID = 'bucketeer-nodejs-server-user-id-2'
+
+export const FEATURE_ID_BOOLEAN = 'feature-nodejs-server-e2e-boolean'
+export const FEATURE_ID_STRING = 'feature-nodejs-server-e2e-string'
+export const FEATURE_ID_INT = 'feature-nodejs-server-e2e-int'
+export const FEATURE_ID_FLOAT = 'feature-nodejs-server-e2e-float'
+export const FEATURE_ID_JSON = 'feature-nodejs-server-e2e-json'
+
+export const GOAL_ID = 'goal-nodejs-server-e2e-1'
+export const GOAL_VALUE = 1.0
